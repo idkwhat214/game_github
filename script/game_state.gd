1106a6c92bbed8af57d463fbae07c4d6a685181f
idkwhat214 <ahanaakirupanantham@gmail.com>
2026-08-05 00:12:08 +0200

    teleportation after dialogue

--- script/game_state.gd
+++ script/game_state.gd
@@ -2,9 +2,14 @@ extends Node
 
 var math_skill := 0
 
-
 func _ready() -> void:
-	pass 
+	pass
+
+func teleport_player(target_position: Vector2) -> void:
+	var player = get_tree().get_first_node_in_group("player") #get player
+	
+	if player: #if player exists
+		player.global_position = target_position #change position, target position can be defined in dialogue file
 
 func _process(_delta: float) -> void:
 	pass
